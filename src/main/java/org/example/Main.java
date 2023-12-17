51bc38b06bf139cb04866f4ef5285d18b9933f8e
Ereb-999 <Naofumi999@yandex.ru>
2023-12-17 13:58:55 +0400

    Chapter final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.*;
+
+
+public class Main {
+    public static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    public static final List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+    public static void main(String[] args) {
+        chapter1();
+        chapter2();
+        chapter3();
+        chapter4();
     }
+        public static void chapter1() {
+            for (int num : nums) {
+                if (num % 2 != 0) {
+                    System.out.println(num);
+                }
+            }
+        }
+    public static void chapter2(){
+        Collections.sort(nums);
+        int minNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != minNum) {
+                System.out.println(num);
+                minNum = num;
+            }
+        }
+    }
+    public static void chapter3(){
+        Set<String> uniString = new HashSet<>(strings);
+        System.out.println(uniString);
+    }
+    public static void chapter4() {
+        Set<String> uniString = new HashSet<>(strings);
+        System.out.println(strings.size() - uniString.size());
+    }
+
+
 }
